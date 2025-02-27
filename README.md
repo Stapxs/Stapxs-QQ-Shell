94eeee44a079547c689f3393beb76a39f22686ca
stapxs <1007028430.stapx@gmail.com>
2025-02-27 08:43:55 +0800

    更新 README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Stapxs-QQ-Shell
+<p align="center">
+  <h2 align="center" style="font-weight: 600">Stapxs QQ Shell</h2>
+ <p align="center">
+  <img src="README/ssqqsh.png" width="400">
+ </p>
+  <p align="center">
+    一个兼容 Napcat 的简易 TUI Obebot 客户端
+    <br />
+    <a href="https://github.com/Stapxs/Stapxs-QQ-Shell/releases" target="blank"><strong>📦️ 下载程序</strong></a>&nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="https://github.com/Stapxs/Stapxs-QQ-Shell/issues" target="blank"><strong>💬 反馈问题</strong></a>
+    <br />
+    <br />
+    <strong>本应用仅供学习交流使用，请勿用于其他用途</strong><br>
+    <strong>版权争议请提出 issue 协商</strong>
+  </p>
+</p>
--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,8 +5,8 @@ PLATFORMS=("linux/amd64" "linux/arm64" "windows/amd64" "darwin/amd64" "darwin/ar
 OUTPUT_DIR="./build"            # 定义输出目录
 APP_NAME="ssqq-shell"           # 定义程序名称
 
-# 通过 git 获取版本号
-VERSION=$(git describe --tags --always)
+# 通过 git 获取短 hash 作为版本号
+VERSION=$(git rev-parse --short HEAD)
 
 mkdir -p "$OUTPUT_DIR"
 
@@ -16,18 +16,18 @@ do
     GOOS=${PLATFORM%/*}
     GOARCH=${PLATFORM#*/}
     OUTPUT_NAME="${APP_NAME}-${VERSION}-${GOOS}-${GOARCH}"
-  
+
     # 为 Windows 添加 .exe 后缀
     if [ "$GOOS" = "windows" ]; then
         OUTPUT_NAME+=".exe"
     fi
-  
+
     # 最终输出路径
     OUTPUT_PATH="${OUTPUT_DIR}/${OUTPUT_NAME}"
-  
+
     echo "正在构建：$GOOS/$GOARCH"
     GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_PATH"
-  
+
     # 检查编译结果
     if [ $? -ne 0 ]; then
         echo "构建失败：$GOOS/$GOARCH"
